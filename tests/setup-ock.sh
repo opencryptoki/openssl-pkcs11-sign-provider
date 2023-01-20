c09b14690ae6cb3ad5bf7bbf8d61ff9cdc0be6ac
Holger Dengler <dengler@linux.ibm.com>
2023-01-20 16:12:31 +0100

    WIP: tests: setup pin file and keys for OCK
    Create a PIN file and an EC key pair. Export all new names to the
    environment.
    
    Signed-off-by: Holger Dengler <dengler@linux.ibm.com>

--- tests/setup-ock.sh
+++ tests/setup-ock.sh
@@ -5,6 +5,13 @@
 [ ! -r "${TESTSDIR}/helpers.sh" ] && exit 77
 source "${TESTSDIR}/helpers.sh"
 
+command -v p11tool > /dev/null || exit 77
+P11TOOL="p11tool --batch --login"
+
+OCK_USER_PIN=${OCK_USER_PIN:-"12345678"}
+OCK_SLOT=${OCK_SLOT:-"3"}
+OCK_TOKEN=${OCK_TOKEN:-"softtok"}
+
 echo "##################################################"
 echo "## Setup test environment (module: ock)"
 echo "##"
@@ -21,11 +28,33 @@ sed -e "s|@libtoollibs[@]|${LIBSPATH}|g" \
     -e "/pkcs11sign-module-init-args.*$/d" \
         "${TESTSDIR}/pkcs11sign.cnf.in" > ${OPENSSL_CONF}
 
+echo "## Generate PKCS#11 pin file"
+PIN_SOURCE=${TMPPDIR}/pinfile.txt
+printf "%s" "${OCK_USER_PIN}" > ${PIN_SOURCE}
+
+echo "## Generate EC Key (if required)"
+URI_KEY_ECDSA="pkcs11:token=${OCK_TOKEN};object=test_ec_secp256r1"
+URI_KEY_ECDSA_PRV="${URI_KEY_ECDSA};type=private"
+URI_KEY_ECDSA_PUB="${URI_KEY_ECDSA};type=public"
+GNUTLS_PIN=${OCK_USER_PIN} \
+${P11TOOL} --list-all \
+	   "${URI_KEY_ECDSA}" 2> /dev/null || \
+GNUTLS_PIN=${OCK_USER_PIN} \
+${P11TOOL} --generate-privkey ecc \
+	   --curve "secp256r1" \
+	   --label "test_ec_secp256r1" \
+	   "pkcs11:token=${OCK_TOKEN}" || \
+exit 99
+
 echo "## Export tests variables to ${TMPPDIR}/testvars"
 tee > ${TMPPDIR}/testvars << DBGSCRIPT
 export TESTSDIR="${TESTSDIR}"
 export TMPPDIR="${BASEDIR}/${TMPPDIR}"
 export OPENSSL_CONF="${BASEDIR}/${OPENSSL_CONF}"
+export PIN_SOURCE=${BASEDIR}/${PIN_SOURCE}
+export URI_KEY_ECDSA="${URI_KEY_ECDSA}?pin-source=${BASEDIR}/${PIN_SOURCE}"
+export URI_KEY_ECDSA_PRV="${URI_KEY_ECDSA_PRV}?pin-source=${BASEDIR}/${PIN_SOURCE}"
+export URI_KEY_ECDSA_PUB="${URI_KEY_ECDSA_PUB}?pin-source=${BASEDIR}/${PIN_SOURCE}"
 DBGSCRIPT
 gen_unsetvars
 
