826f6c898679b9cf297d4d46e89b90ad6550361b
Holger Dengler <dengler@linux.ibm.com>
2023-04-21 14:11:37 +0200

    WIP: test: setup: generate RSA keys
    Prepare RSA keys for testing.
    
    Signed-off-by: Holger Dengler <dengler@linux.ibm.com>

--- tests/helpers.sh
+++ tests/helpers.sh
@@ -8,6 +8,67 @@ gen_unsetvars() {
     >> "${TMPPDIR}/unsetvars"
 }
 
+ensure_ca_key_cert() {
+	local ca_prv="$1"
+	local ca_crt="$2"
+
+	# skip, if key/cert already exists
+	test -r "${ca_prv}" -a -r "${ca_crt}" && return
+
+	# create ca key/certificate (selfsigned)
+	openssl req						\
+		-newkey ec -pkeyopt ec_paramgen_curve:secp384r1	\
+		-nodes -keyout "${ca_prv}"			\
+		-x509 -sha256					\
+		-days 3650					\
+		-subj '/CN=test-ca'				\
+		-outform PEM -out "${ca_crt}"
+}
+
+generate_rsa_tls_certificates() {
+	local ca_prv="$1"
+	local ca_crt="$2"
+	local server_prv="$3"
+	local server_pub="$4"
+	local server_crt="$5"
+
+	local tmppdir=$(dirname "${server_crt}")
+
+	cat > "${tmppdir}/server.v3.ext" << EOF
+basicConstraints=critical,CA:FALSE
+keyUsage = digitalSignature
+extendedKeyUsage = serverAuth
+EOF
+
+	# create ca key/certificate (selfsigned)
+	ensure_ca_key_cert "${ca_prv}" "${ca_crt}"
+
+	# create server key/csr (self-signed)
+	openssl req						\
+		-newkey rsa:4096				\
+		-nodes -keyout ${server_prv}			\
+		-new -subj '/CN=localhost'			\
+		-outform PEM -out "${tmppdir}/tmp.csr"
+
+	# sign server certificate (ca-signed)
+	openssl x509						\
+		-req -sha256 -in "${tmppdir}/tmp.csr"		\
+		-CAkey "${ca_prv}" -CA "${ca_crt}"		\
+		-CAcreateserial					\
+		-days 365					\
+		-extfile "${tmppdir}/server.v3.ext"		\
+		-outform PEM -out "${server_crt}"
+
+	# export public server key
+	openssl pkey 						\
+		-pubout						\
+		-in ${server_prv}				\
+		-outform PEM -out ${server_pub}
+
+	# cleanup
+	rm -f "${tmppdir}/tmp.csr" "${tmppdir}/server.v3.ext"
+}
+
 generate_ec_tls_certificates() {
 	local ca_prv="$1"
 	local ca_crt="$2"
@@ -24,13 +85,7 @@ extendedKeyUsage = serverAuth
 EOF
 
 	# create ca key/certificate (selfsigned)
-	openssl req						\
-		-newkey ec -pkeyopt ec_paramgen_curve:secp384r1	\
-		-nodes -keyout "${ca_prv}"			\
-		-x509 -sha256					\
-		-days 3650					\
-		-subj '/CN=test-ca'				\
-		-outform PEM -out "${ca_crt}"
+	ensure_ca_key_cert "${ca_prv}" "${ca_crt}"
 
 	# create server key/csr (self-signed)
 	openssl req						\
@@ -59,6 +114,19 @@ EOF
 }
 
 generate_payload() {
+	for length in 256 512; do
+		DATA="${TMPPDIR}/random-${length}.bin"
+
+		dd if=/dev/random				\
+		   of="${DATA}"					\
+		   bs=1 count=${length}
+
+		openssl sha256					\
+			-binary					\
+			-out "${DATA}.sha256"			\
+			"${DATA}"
+	done
+
 	for length in 1 64; do
 		DATA="${TMPPDIR}/random-${length}k.bin"
 
--- tests/setup-ock.sh
+++ tests/setup-ock.sh
@@ -53,7 +53,7 @@ printf "%s" "${OCK_USER_PIN}" > ${PIN_SOURCE} \
 || exit 99
 
 #######################################
-echo "## Generate CA key/cert and server key/cert"
+echo "## Generate CA key/cert and server key/cert (ec)"
 LABEL="test_ec_secp384r1"
 FILE_PEM_CA_PRV="${TMPPDIR}/ca_key.prv"
 FILE_PEM_CA_CRT="${TMPPDIR}/ca_cert.crt"
@@ -65,7 +65,7 @@ generate_ec_tls_certificates \
 	${FILE_PEM_ECDSA_PRV} ${FILE_PEM_ECDSA_PUB} ${FILE_PEM_ECDSA_CRT}
 
 #######################################
-echo "## Re-import server keys"
+echo "## Re-import server keys (ec)"
 URI_TOKEN="pkcs11:token=${OCK_TOKEN}"
 URI_KEY_ECDSA="${URI_TOKEN};object=${LABEL}"
 URI_KEY_ECDSA_PRV="${URI_KEY_ECDSA};type=private"
@@ -85,6 +85,41 @@ ${P11TOOL} --write --label ${LABEL}		\
 GNUTLS_PIN=${OCK_USER_PIN}			\
 ${P11TOOL} --write --label ${LABEL}		\
 	   --load-pubkey=${FILE_PEM_ECDSA_PUB}	\
+	   "${URI_TOKEN}" 2> /dev/null		\
+|| exit 99
+
+#######################################
+echo "## Generate CA key/cert and server key/cert (rsa)"
+LABEL="test_rsa_4k"
+FILE_PEM_CA_PRV="${TMPPDIR}/ca_key.prv"
+FILE_PEM_CA_CRT="${TMPPDIR}/ca_cert.crt"
+FILE_PEM_RSA4K_PRV="${TMPPDIR}/${LABEL}_key.prv"
+FILE_PEM_RSA4K_PUB="${TMPPDIR}/${LABEL}_key.pub"
+FILE_PEM_RSA4K_CRT="${TMPPDIR}/${LABEL}_cert.crt"
+generate_rsa_tls_certificates \
+	${FILE_PEM_CA_PRV} ${FILE_PEM_CA_CRT} \
+	${FILE_PEM_RSA4K_PRV} ${FILE_PEM_RSA4K_PUB} ${FILE_PEM_RSA4K_CRT}
+
+#######################################
+echo "## Re-import server keys (rsa)"
+URI_KEY_RSA4K="${URI_TOKEN};object=${LABEL}"
+URI_KEY_RSA4K_PRV="${URI_KEY_RSA4K};type=private"
+URI_KEY_RSA4K_PUB="${URI_KEY_RSA4K};type=public"
+
+GNUTLS_PIN=${OCK_USER_PIN}			\
+${P11TOOL} --delete				\
+	   "${URI_KEY_RSA4K}" 2> /dev/null
+
+GNUTLS_PIN=${OCK_USER_PIN}			\
+${P11TOOL} --write --label ${LABEL}		\
+	   --mark-private			\
+	   --load-privkey=${FILE_PEM_RSA4K_PRV}	\
+	   "${URI_TOKEN}" 2> /dev/null		\
+|| exit 99
+
+GNUTLS_PIN=${OCK_USER_PIN}			\
+${P11TOOL} --write --label ${LABEL}		\
+	   --load-pubkey=${FILE_PEM_RSA4K_PUB}	\
 	   "${URI_TOKEN}" 2> /dev/null		\
 || exit 99
 
@@ -112,6 +147,12 @@ export FILE_PEM_ECDSA_CRT="${BASEDIR}/${FILE_PEM_ECDSA_CRT}"
 export URI_KEY_ECDSA="${URI_KEY_ECDSA}?pin-source=${BASEDIR}/${PIN_SOURCE}"
 export URI_KEY_ECDSA_PRV="${URI_KEY_ECDSA_PRV}?pin-source=${BASEDIR}/${PIN_SOURCE}"
 export URI_KEY_ECDSA_PUB="${URI_KEY_ECDSA_PUB}?pin-source=${BASEDIR}/${PIN_SOURCE}"
+export FILE_PEM_RSA4K_PRV="${BASEDIR}/${FILE_PEM_RSA4K_PRV}"
+export FILE_PEM_RSA4K_PUB="${BASEDIR}/${FILE_PEM_RSA4K_PUB}"
+export FILE_PEM_RSA4K_CRT="${BASEDIR}/${FILE_PEM_RSA4K_CRT}"
+export URI_KEY_RSA4K="${URI_KEY_RSA4K}?pin-source=${BASEDIR}/${PIN_SOURCE}"
+export URI_KEY_RSA4K_PRV="${URI_KEY_RSA4K_PRV}?pin-source=${BASEDIR}/${PIN_SOURCE}"
+export URI_KEY_RSA4K_PUB="${URI_KEY_RSA4K_PUB}?pin-source=${BASEDIR}/${PIN_SOURCE}"
 DBGSCRIPT
 test $? -eq 0 \
 || exit 99
